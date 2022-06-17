b93946d9bf9115a3e0325bd7d11ac3291724db8b
matheusoliv23 <matheushenri1996@gmail.com>
2022-06-17 08:20:22 -0300

    feat: AuthProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,15 @@
-import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import { AuthProvider } from "../contexts/Auth/AuthProvider";
 
 // Use the <SessionProvider> to improve performance and allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider
-      // Provider options are not required but can be useful in situations where
-      // you have a short session maxAge time. Shown here with default values.
-      session={pageProps.session}
+    <AuthProvider
+    // Provider options are not required but can be useful in situations where
+    // you have a short session maxAge time. Shown here with default values.
     >
       <Component {...pageProps} />
-    </SessionProvider>
+    </AuthProvider>
   );
 }
